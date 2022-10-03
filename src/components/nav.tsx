2185b76bf4666836118dc98ee9b3550f995ed3cf
UnderDog01a <113715907+TheUnderDog01a@users.noreply.github.com>
2022-10-03 21:27:14 +0200

    fixed navbar

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link" 
 export function Navbar() {
     return (
         <nav className="navBar">
             <ul>
-                <li className="tabActive"><a href="/">Home</a></li>
-                <li className="tabActive"><a href="/teams">Teams</a></li>
-                <li className="tabActive"><a href="/goals">Goals</a></li>
-                <li className="tabActive"><a href="/partners">Partners</a></li>
-                <li className="tabActive"><a href="/contact">Contact</a></li>
+                <li className="tabActive"><Link href="/">Home</Link></li>
+                <li className="tabActive"><Link href="/teams">Teams</Link></li>
+                <li className="tabActive"><Link href="/goals">Goals</Link></li>
+                <li className="tabActive"><Link href="/partners">Partners</Link></li>
+                <li className="tabActive"><Link href="/contact">Contact</Link></li>
+                <li className="tabActive"><Link href="/shop">Shop</Link></li>
             </ul>
         </nav>
     );
