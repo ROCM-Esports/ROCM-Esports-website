556ed654e8f15480bc49c85c4a86f4546f818c98
302869442 <j.vdv@outlook.com>
2022-10-16 15:19:02 +0200

    add checkout page and payments

---
+++ src/pages/api/checkout/checkout_session.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Stripe from 'stripe';
+const stripe = new Stripe(<string>process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2022-08-01',
+  typescript: true,
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: 'price_1LotNWFy83UmjqiZEmWH8rsn',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        currency: 'eur',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      });
+      res.redirect(303, <string>session.url);
+    }
+    catch (error) {
+      res.status(error.statusCode || 500).json({ statusCode: error.statusCode, message: error.message });
+    }
+  }
+  else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
